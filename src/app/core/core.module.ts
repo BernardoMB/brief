534f9e29043db2ebe404184563123b443e990faa
unknown <enrifransch@gmail.com>
2018-02-13 11:56:15 -0600

    modal para registrar added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,25 +1,26 @@
-import { ModalModule } from 'ng-bootstrap/modal';
-import { LOCALE_ID, ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // In order to use two way binding
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { SharedModule } from '../shared/shared.module';
-import { MainComponent } from './containers/main/main.component';
-import { LaunchpadComponent } from './containers/launchpad/launchpad.component';
-import { ClickOutsideModule } from 'ng-click-outside';
-import { CookieModule } from 'ngx-cookie';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { TooltipConfig, TooltipModule } from 'ngx-bootstrap/tooltip';
-import { ToastyModule } from 'ng2-toasty';
-import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
-import { DndModule } from 'ng2-dnd';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { OfferComponent } from './containers/offer/offer.component';
-import { AuthService } from './services/auth0.service';
-import { Launchpad2Component } from './containers/launchpad2/launchpad2.component';
+import {ModalModule} from 'ng-bootstrap/modal';
+import {LOCALE_ID, ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms'; // In order to use two way binding
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {SharedModule} from '../shared/shared.module';
+import {MainComponent} from './containers/main/main.component';
+import {LaunchpadComponent} from './containers/launchpad/launchpad.component';
+import {ClickOutsideModule} from 'ng-click-outside';
+import {CookieModule} from 'ngx-cookie';
+import {ButtonsModule} from 'ngx-bootstrap/buttons';
+import {TooltipConfig, TooltipModule} from 'ngx-bootstrap/tooltip';
+import {ToastyModule} from 'ng2-toasty';
+import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
+import {DndModule} from 'ng2-dnd';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './components/header/header.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {OfferComponent} from './containers/offer/offer.component';
+import {AuthService} from './services/auth0.service';
+import {Launchpad2Component} from './containers/launchpad2/launchpad2.component';
+import {RegisterModalComponent} from '../shared/components/register-modal/register-modal.component';
 
 @NgModule({
   imports: [
@@ -62,16 +63,16 @@ import { Launchpad2Component } from './containers/launchpad2/launchpad2.componen
   ]
 })
 export class CoreModule {
-  constructor(@Optional() @SkipSelf() parentModule: CoreModule ) {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     if (parentModule) {
-      throw new Error ('Error, core module already exists.');
+      throw new Error('Error, core module already exists.');
     }
   }
 
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: CoreModule,
-      providers: [{provide: LOCALE_ID, useValue : 'es-MX'}, TooltipConfig, AuthService]
+      providers: [{provide: LOCALE_ID, useValue: 'es-MX'}, TooltipConfig, AuthService]
     };
   }
- }
+}
